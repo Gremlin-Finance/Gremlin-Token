17a354dfced8fb9f2e6eae47c67af89eecbbe86c
Gremlin-Finance <103136391+Gremlin-Finance@users.noreply.github.com>
2022-04-06 15:28:22 -0400

    internal

---
+++ Contracts/interface/IGRM.sol
@@ -0,0 +1,8 @@
+pragma solidity =0.6.6;
+
+//IERC20
+interface IGRM {
+    function balanceOf(address account) external view returns (uint);
+
+    function transfer(address dst, uint rawAmount) external returns (bool);
+}
